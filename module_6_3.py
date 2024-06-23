a594250f06d157a7fd1881877fb3cc4b585ec916
alexwlad3885 <alexwlad3885@yandex.ru>
2024-06-23 14:32:43 +0300

    Исправленное домашнее задание по теме "Множественное наследование"

--- module_6_3.py
+++ module_6_3.py
@@ -7,38 +7,44 @@ class Vehicle:
     """
     класс Vehicle транспортных средств, родительский, базовый
     """
-    def __init__(self, vehicle_type=None):
-        self.vehicle_type: str = vehicle_type
+    vehicle_type = None
 
 
 class Car:
     """
     класс Car машин, родительский, базовый
     """
-    price: int = 1000000
+    price = 1000000
 
-    def __init__(self, price, horsepower_for_car):
-        self.price = price
-        self.horsepower_for_car: int = horsepower_for_car
+    def __init__(self, price, power):
+        self.power = power
 
     def horce_powers(self):
-        print(f'У автомобиля {self.horsepower_for_car} лошадинных сил')
+
+        h_power: float = self.power * 1.3596
+        return h_power
 
 
-class Nissan(Vehicle, Car):
+class Nissan(Car, Vehicle):
     """
     класс Nissan модель автомобиля, наследник класса Car и Vehicle
     """
-    def __init__(self, vehicle_type, price, horsepower_for_car):
-        super().__init__(vehicle_type)
-        Car.__init__(self, price, horsepower_for_car)
-        Car.horce_powers(self)
+    def __init__(self, price, vehicle_type, power):
+        super().__init__(price, power)
+        super().horce_powers()
+        Vehicle.__init__(self.vehicle_type)
+        self.price = price
+        self.vehicle_type = vehicle_type
+        self.power = power
 
 
 if __name__ == '__main__':
 
-    vehicle = Vehicle()
-    car = Car(0, 0)
-    nissan = Nissan('автомобиль', 2500000, 267)
+    nissan = Nissan('автомобиль', 2500000, 150)
     print(nissan.vehicle_type)
     print(nissan.price)
+    print(nissan.power)
+    print(nissan.horce_powers())
+
+    print(Vehicle.vehicle_type)
+    print(Car.price)
